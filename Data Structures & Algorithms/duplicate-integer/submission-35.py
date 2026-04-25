2c610729f955971dce509fc4369b50197cb977fe
Xia Jie Ou <109823170+xiajieou@users.noreply.github.com>
2026-04-25 15:24:03 -0400

    Add: duplicate-integer - submission-35

---
+++ Data Structures & Algorithms/duplicate-integer/submission-35.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        my_set = set()
+
+        for i in nums:
+            if i in my_set:
+                return True
+            my_set.add(i)
+        return False
